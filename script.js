6ce7823f31fe88b48e19406163f7a79b05755762
thisispete16 <petelastcallhockey@gmail.com>
2026-01-07 18:10:59 -0500

    Update script.js
    fixed the pic names

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // List of JPEG images
 const images = [
-  "images/image1.jpg",
-  "images/image2.jpg",
-  "images/image3.jpg",
-  "images/image4.jpg"
+  "images/dailies (1).jpg",
+  "images/dailies (2).jpg",
+  "images/dailies (3).jpg",
+  "images/dailies (4).jpg"
 ];
 
 // Pick a random image
